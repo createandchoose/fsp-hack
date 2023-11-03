08e8a3cdaa5c235ec8c36776c31e1b071722faee
Aytal Popov <aytal.popov2002@yandex.ru>
2023-11-03 22:07:02 +0900

    asd

---
+++ test.py
@@ -0,0 +1,26 @@
+import psycopg2
+
+try:
+    # Устанавливаем соединение с базой данных
+    conn = psycopg2.connect(
+        host="80.90.185.102",
+        database="default_db",
+        user="admin",
+        password = "fsphack1"
+    )
+    
+    cursor = conn.cursor()
+    cursor.execute("SELECT version();")
+    db_version = cursor.fetchone()
+
+    if db_version:
+        print("Вход произошел. Версия PostgreSQL:", db_version)
+    else:
+        print("Нет входа")
+
+    cursor.close()
+    conn.close()
+
+except psycopg2.Error as error:
+    print("Ошибка при соединении с базой данных:", error)
+    print("Нет входа")
